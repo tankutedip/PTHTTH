dc84c8f87576dd3c69662e313a67ccf2ae386275
tankutedip <72064284+tankutedip@users.noreply.github.com>
2020-10-06 13:37:43 +0700

    Create Bai1.java

---
+++ Lab3/Bai1.java
@@ -0,0 +1,23 @@
+package lab3;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class TimDCIP {
+
+	public static void main(String[] args) {
+		try {
+			InetAddress host = InetAddress.getByName("www.google.com");
+			String hostName = host.getHostName();
+			System.out.println("Host name: "+hostName);
+			System.out.println("Dia Chi IP: "+host.getHostAddress());
+		}
+		catch(UnknownHostException e)
+		{
+			System.out.println("Khong tim thay dia chi");
+			return;
+		}
+
+	}
+
+}
