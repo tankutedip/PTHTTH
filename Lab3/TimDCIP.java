9aa722ab6baf338c1db3018a6781f51a4a91e3ad
tankutedip <72064284+tankutedip@users.noreply.github.com>
2020-10-10 10:06:39 +0700

    Update TimDCIP.java

--- Lab3/TimDCIP.java
+++ Lab3/TimDCIP.java
@@ -7,7 +7,7 @@ public class TimDCIP {
 
 	public static void main(String[] args) {
 		try {
-			InetAddress host = InetAddress.getByName("www.google.com");
+			InetAddress host = InetAddress.getByName("iuh.edu.vn");
 			String hostName = host.getHostName();
 			System.out.println("Host name: "+hostName);
 			System.out.println("Dia Chi IP: "+host.getHostAddress());
